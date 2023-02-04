b10b5c7ee72607512eec447beae1de616f0a7fc2
Michał Kaniowski <kaniowskimichal2@gmail.com>
2023-02-04 11:58:42 +0100

    Finished main functionalities of the quiz app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,97 @@ import {nanoid} from "nanoid";
 export default function App() {
     const [isHomeScreen, setIsHomeScreen] = useState(true);
     const [questions, setQuestions] = useState([]);
+    const [isQuizFinished, setIsQuizFinished] = useState(false);
+    let chosenAnswersArray = [];
+    const [correctAnswersCount, setCorrectAnswerCount] = useState(0);
 
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium")
-        .then(res => res.json())
-        .then(data => setQuestions(data.results))      
+        getQuestions();
     }, []);
 
-    console.log(questions);
-
-    function handleButtonClick() {
-        setIsHomeScreen(prevValue => !prevValue);
+    function getQuestions() {
+        fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium")
+        .then(res => res.json())
+        .then(data => setQuestions(data.results));
     }
+
+    function handleHomeButtonClick() {
+        setIsHomeScreen(false);
+    }
+
+    function createAnswerObject(indexOfChosenAnswer, indexOfCorrectAnswer, idOfChosenQuestion) {
+        return {
+            questionId: idOfChosenQuestion,
+            chosenAnswerIndex: indexOfChosenAnswer,
+            correctAnswerIndex: indexOfCorrectAnswer
+        }
+    }
+
+    // const [checkedAnswers, setCheckedAnswers] = [];
+    // console.log(checkedAnswers);
+
+    // useEffect(() => {
+    //     localStorage.setItem("areAnswerChecked", checkedAnswers);
+    // }, [checkedAnswers])
+
+    function handleAnswerChange(indexOfChosenAnswer, indexOfCorrectAnswer, idOfChosenQuestion, areAnswersChecked) {
+        let isInTheArray = false;
+        let indexOfItemToReplace;
+
+        // setCheckedAnswers(prevAnswers => {
+        //     return [
+        //         ...prevAnswers,
+        //         areAnswersChecked
+        //     ]
+        // });
+
+        chosenAnswersArray.map((answer, index) => {
+            if (idOfChosenQuestion == answer.questionId) {
+                isInTheArray = true;
+                indexOfItemToReplace = index;
+            }
+        });
+
+        if (!isInTheArray) {
+            chosenAnswersArray.push(createAnswerObject(indexOfChosenAnswer, indexOfCorrectAnswer, idOfChosenQuestion));
+        } else {
+            chosenAnswersArray[indexOfItemToReplace] = createAnswerObject(indexOfChosenAnswer, indexOfCorrectAnswer, idOfChosenQuestion);
+        }
+        // console.log(chosenAnswersArray);
+    }
+
+    function countCorrectAnswers() {
+        let count = 0;
+        chosenAnswersArray.map(answer => {
+            answer.chosenAnswerIndex === answer.correctAnswerIndex && count++;
+        });
+
+        setCorrectAnswerCount(count);
+    }
+
+    function handleAnswersSubmit() {
+        setIsQuizFinished(true);
+        countCorrectAnswers(chosenAnswersArray);
+    }
+
+    function restartGame() {
+        chosenAnswersArray = [];
+        getQuestions();
+        setIsQuizFinished(false);
+    } 
 
     const questionElements = questions.map(question => {
         return <QuestionElement 
             key={nanoid()} 
             id={nanoid()} 
             text={question.question} 
-            answers={[...question.incorrect_answers, question.correct_answer]}
+            incorrect_answers={question.incorrect_answers}
+            correct_answer={question.correct_answer}
+            handleAnswerChange={handleAnswerChange}
+            isQuizFinished={isQuizFinished}
         />
     });
+
 
     return <main className={isHomeScreen ? "flex-centered" : "questions-container"}>
         {isHomeScreen 
@@ -33,13 +103,30 @@ export default function App() {
             <div className="home-container">
                 <h1 className="home-heading">Quizzical</h1>
                 <p className="home-description">Some description if needed</p>
-                <button onClick={handleButtonClick} className="home-button">Start quiz</button>
+                <button onClick={handleHomeButtonClick} className="home-button">Start quiz</button>
             </div> 
             :
             <form>
                 {questionElements}
+                <div className="score-container">
+                    {isQuizFinished && <p>You scored {correctAnswersCount}/5 answers</p>}
+                    {!isQuizFinished ? <button 
+                        onClick={handleAnswersSubmit} 
+                        type="button" 
+                        className="home-button"
+                    >
+                        Check answers
+                    </button> :
+                    <button
+                        onClick={restartGame}
+                        type="button"
+                        className="home-button"
+                    >
+                        Play again
+                    </button>
+                    }
+                </div>
             </form>
         }
-        
     </main>
 }
--- src/components/QuestionElement.jsx
+++ src/components/QuestionElement.jsx
@@ -1,28 +1,71 @@
 import React, {useState, useEffect} from "react";
-import AnswerButton from "./AnswerButton";
 import {nanoid} from "nanoid";
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+}
+
 export default function QuestionElement(props) {
-    const [chosenAnswerId, setChosenAnswerId] = useState("");
+    let allAnswers = [...props.incorrect_answers, props.correct_answer]; 
+    allAnswers = shuffleArray(allAnswers);
+    const [isQuizFinished, setIsQuizFinished] = useState(props.isQuizFinished);
+
+    const answerObjects = allAnswers.map((answer, index) => {
+        return {
+            id: nanoid(),
+            text: answer,
+            isCorrect: answer === props.correct_answer,
+            isChecked: false,
+            index: index
+        }
+    });
+
+    const areAnswersCheckedArray = [false, false, false, false, false];
+    const [areAnswersChecked, setAreAnswersChecked] = useState(() => areAnswersCheckedArray);
+
+    // console.log(areAnswersChecked);
+
+    //we need this useState so answers don't refresh on every refresh of the app
+    const [answers, setAnswers] = useState(() => answerObjects); 
+    const [chosenAnswerId, setChosenAnswerId] = useState(() => "");
+
+    function getCorrectAnswerIndex() {
+        return answers.indexOf(answers.find(object => object.text == props.correct_answer));
+    }
 
     function handleAnswerChange(chosenAnswerId) {
-        console.log(chosenAnswerId);
-        setChosenAnswerId(prevValue => chosenAnswerId)
+        const indexOfChosenAnswer = answers.indexOf(answers.find(object => object.id == chosenAnswerId));
+        const indexOfCorrectAnswer = getCorrectAnswerIndex();
+
+        areAnswersChecked[indexOfChosenAnswer] = true;
+
+        props.handleAnswerChange(indexOfChosenAnswer, indexOfCorrectAnswer, props.id, areAnswersChecked); 
+        setChosenAnswerId(chosenAnswerId);
     }
+
+    const buttonAnswers = answers.map((answer, index) => {
+        // console.log(areAnswersChecked[index]);
+        // console.log(answer);
+        return <button 
+            key={nanoid()} 
+            onClick={() => handleAnswerChange(answer.id)} 
+            className={`answer-button ${chosenAnswerId === answer.id ? "active" : ""} ${isQuizFinished && answer.isCorrect ? "correct" : ""}`}
+            >
+            {answer.text}
+        </button>
+    });
+
+    console.log(chosenAnswerId);
 
     return <div className="question-element">
         <h3 className="question-element-heading">{props.text}</h3>
-
-        {props.answers.map(answer => {
-            const answerKey = nanoid();
-            return <AnswerButton 
-                key={nanoid()}
-                id={answerKey}
-                questionAnswer={answer} 
-                isActive={chosenAnswerId === answerKey ? true : false}
-                handleClick={handleAnswerChange}  
-                />
-        })}
+        {buttonAnswers}
         <hr style={{color: "#DBDEF0"}}></hr>
     </div>
 }
